1edccd979f30ac9c0af43be26a21e0e66a51d6d4
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-13 19:12:35 -0800

    server outline done

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var http = require('http');
-
-function onRequest(request, response) {
-    console.log('request received')
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.write('hello world');
-    response.end();
+function start() {
+    function onRequest(request, response) {
+        console.log('request received')
+        response.writeHead(200, {'Content-Type': 'text/plain'});
+        response.write('hello world');
+        response.end();
+    }
+    http.createServer(onRequest).listen(8888);
+    console.log('server has started')
 }
 
-http.createServer(onRequest).listen(8888);
-
-console.log('server has started.');
+exports.start = start;
