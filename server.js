fbb7e48ffac63c0e2d3e3dd51751c9dd92a321de
Tyler Corbett <thetylercorbett@gmail.com>
2019-01-13 19:23:47 -0800

    router and server tweaks

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-var http = require('http');
+const http = require('http');
+const url = require('url');
+
 function start() {
     function onRequest(request, response) {
-        console.log('request received')
+        const pathname = url.parse(request.url).pathname;
+        console.log('request for ' + pathname + ' received.')
         response.writeHead(200, {'Content-Type': 'text/plain'});
         response.write('hello world');
         response.end();
